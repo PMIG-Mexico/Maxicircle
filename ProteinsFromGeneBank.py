2c2f8b8d6f68cfc77216eaba1eff2d9bb469d5c6
said3427 <smunoz@grupoabraxas.com>
2016-05-11 22:43:14 -0500

    update

--- ProteinsFromGeneBank.py
+++ ProteinsFromGeneBank.py
@@ -10,9 +10,10 @@ from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import ProteinAlphabet
 import argparse
 
-class ProteinSeqFromGenBank:
+class ProteinsFromGenBank:
 	def __init__(self,GeneBank_file):
 		fasta_out=[]
+		genes_out=[]
 		for seq_record in SeqIO.parse(GeneBank_file,"gb"):
 			name="".join(seq_record.name)
 			organism="".join(seq_record.annotations['organism'])
@@ -23,15 +24,17 @@ class ProteinSeqFromGenBank:
 					if 'product' in feature.qualifiers:
 						product=feature.qualifiers['product']
 					else:
-						product=['']*len(proteinSequence)
+						product=['unknown']*len(proteinSequence)
 					for i in range(len(proteinSequence)):
 						description='|'+organism+'|'+product[i]
 						out=SeqRecord(seq=Seq(proteinSequence[i],ProteinAlphabet()),id=proteinID[i],name=proteinID[i],description=description)
 						fasta_out.append(out)
-		self.sequences=fasta_out
+		self.sequencesAA=fasta_out
 
-	def writeFasta(self,output_file):
-		fasta_out=self.sequences
+
+	def writeFAA(self,output_file):
+		fasta_out=self.sequencesAA
+		output_file=output_file+'.proteins.faa'
 		output_handle = open(output_file, "w")
 		SeqIO.write(fasta_out, output_handle, "fasta")
 		output_handle.close()
@@ -44,10 +47,9 @@ def main():
 	args = parser.parse_args()
 	GeneBank_file=args.gbk
 	output_file=args.out
-
-	fasta=ProteinSeqFromGenBank(GeneBank_file)
-	fasta.writeFasta(output_file)
-	print(str(len(fasta.sequences))+" Proteins extracted")
+	fasta=ProteinsFromGenBank(GeneBank_file)
+	fasta.writeFAA(output_file)
+	print(str(len(fasta.sequencesAA))+" Proteins extracted\n")
 
 if __name__ == "__main__":
 	main()
