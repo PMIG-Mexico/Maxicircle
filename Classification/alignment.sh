d4351e3bbe1424f291553647909486e629742753
said3427 <smunoz@grupoabraxas.com>
2016-05-17 00:37:54 -0500

    Manually filtered sequences

--- Classification/alignment.sh
+++ Classification/alignment.sh
@@ -4,7 +4,7 @@ for i in $(ls *.faa);
 do 
 clustalo  --output-order=tree-order  -i $i -o $i.aln; 
 makeblastdb -in $i -dbtype 'prot' -out $i ;
-blastp -out $i.xml -outfmt 5 -query "/Users/Said/Github/Maxicircle/Leishmania_mexicana_maxicircle.fasta" -db $i ;
+blastp -out $i.xml -outfmt 5 -query "../Leishmania_mexicana_maxicircle.fasta" -db $i ;
 done;
 
 mkdir ALN
@@ -16,7 +16,7 @@ for i in $(ls *.fna);
 do 
 clustalo  --output-order=tree-order  -i $i -o $i.aln; 
 makeblastdb -in $i -dbtype 'nucl' -out $i ;
-blastn -out $i.xml -outfmt 5 -query "/Users/Said/Github/Maxicircle/Leishmania_mexicana_maxicircle.fasta" -db $i ;
+blastn -out $i.xml -outfmt 5 -query "../Leishmania_mexicana_maxicircle.fasta" -db $i ;
 done;
 
 mkdir ALN
